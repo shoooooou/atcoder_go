84c007d1ed5a7dc08b7b411be7176b8ea0e618a2
s.nakamura <shotenba@gmail.com>
2024-06-21 22:59:44 +0900

    典型90の044を途中コミット

---
+++ tenkei90/044/ans.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// スライスの合計値を算出する
+func sumSlice(numbers []int) int {
+	sum := 0
+	for _, n := range numbers {
+		sum += n
+	}
+	return sum
+}
+
+// 最大公約数を算出する
+func gcd(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	return gcd(b, a%b)
+}
+
+// 絶対値を算出する
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+// スライスの要素を入れ替える
+func swap(array []int, i, j int) {
+	array[i], array[j] = array[j], array[i]
+}
+
+// 順列を生成する
+func generate(n int, array []int, result *[][]int) {
+	if n == 1 {
+		tmp := make([]int, len(array))
+		copy(tmp, array)
+		*result = append(*result, tmp)
+	} else {
+		for i := 0; i < n; i++ {
+			generate(n-1, array, result)
+			if n%2 == 1 {
+				swap(array, 0, n-1)
+			} else {
+				swap(array, i, n-1)
+			}
+		}
+	}
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+	scanner.Scan()
+	q, _ := strconv.Atoi(scanner.Text())
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		scanner.Scan()
+		a[i], _ = strconv.Atoi(scanner.Text())
+	}
+
+	shiftCnt := 0
+	for i := 0; i < q; i++ {
+		scanner.Scan()
+		t, _ := strconv.Atoi(scanner.Text())
+
+		scanner.Scan()
+		x, _ := strconv.Atoi(scanner.Text())
+		if x != n {
+			x = (x - 1 + shiftCnt) % n
+		}
+
+		scanner.Scan()
+		y, _ := strconv.Atoi(scanner.Text())
+		y = (y + shiftCnt) % n
+		if y != n {
+			y = (y - 1 + shiftCnt) % n
+		}
+
+		if t == 1 {
+			swap(a, x, y)
+		} else if t == 2 {
+			shiftCnt = (shiftCnt - 1 + n) % n
+		} else {
+			fmt.Println(a[x])
+		}
+	}
+}
