12b2b9990df36d4c90fa1f5399dc562be69c7e88
s.nakamura <shotenba@gmail.com>
2024-06-22 16:23:24 +0900

    典型90の050を解答

---
+++ tenkei90/050/ans.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// スライスの合計値を算出する
+func sumSlice(numbers []int) int {
+	sum := 0
+	for _, n := range numbers {
+		sum += n
+	}
+	return sum
+}
+
+// 最大公約数を算出する
+func gcd(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	return gcd(b, a%b)
+}
+
+// 絶対値を算出する
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+// スライスの要素を入れ替える
+func swap(array []int, i, j int) {
+	array[i], array[j] = array[j], array[i]
+}
+
+// 順列を生成する
+func generate(n int, array []int, result *[][]int) {
+	if n == 1 {
+		tmp := make([]int, len(array))
+		copy(tmp, array)
+		*result = append(*result, tmp)
+	} else {
+		for i := 0; i < n; i++ {
+			generate(n-1, array, result)
+			if n%2 == 1 {
+				swap(array, 0, n-1)
+			} else {
+				swap(array, i, n-1)
+			}
+		}
+	}
+}
+func inputValue() (int, int) {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	scanner.Scan()
+	a, _ := strconv.Atoi(scanner.Text())
+	scanner.Scan()
+	b, _ := strconv.Atoi(scanner.Text())
+	return a, b
+}
+
+func main() {
+	const mod = 1e9 + 7
+	n, l := inputValue()
+	cnt := make([]int, n+1)
+	cnt[0] = 1
+	for i := 1; i <= n; i++ {
+		if i<l{
+			cnt[i] = cnt[i-1]%mod
+		}else{
+			cnt[i] =(cnt[i-1]+cnt[i-l])%mod
+		}
+	}
+	fmt.Println(cnt[n])
+}
