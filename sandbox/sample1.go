4079206b501074164026eb99ab6357dfc4a95abc
s.nakamura <shotenba@gmail.com>
2024-06-24 00:54:36 +0900

    典型90の064を解答

--- sandbox/sample1.go
+++ sandbox/sample1.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func solve() {
-	var n  = "Hello, World!"
-	fmt.Printf(n)
+func goroutine(s []int, c chan int) {
+	sum := 0
+	for i, v := range s {
+		fmt.Printf("i:%v,v:%v\n",i,v)
+		sum += v
+	}
+	c <- sum
 }
 
 func main() {
-	solve()
+	s := []int{1, 2, 3, 4, 5}
+	ss := []int{2, 4, 6, 8, 10}
+	c := make(chan int)
+
+	go goroutine(s, c)
+	go goroutine(ss, c)
+	x := <-c
+	y := <-c
+	fmt.Println(x)
+	fmt.Println(y)
 }
