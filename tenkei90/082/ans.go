3b4018b6b02c7af0dd54b030ce0a5bd155c57f70
s.nakamura <shotenba@gmail.com>
2024-07-20 13:39:27 +0900

    典型問題082を回答

--- tenkei90/082/ans.go
+++ tenkei90/082/ans.go
@@ -8,52 +8,32 @@ import (
 	"strconv"
 )
 
-// スライスの合計値を算出する
-func sumSlice(numbers []int64) int64 {
-	var sum int64 = 0
-	for _, n := range numbers {
-		sum += n
-	}
-	return sum
+const mod = 1000000007
+
+// 総和を算出する
+func sum(a int64) int64 {
+	return ((a % mod) * ((a + 1) % mod) / 2) % mod
 }
 
-// 最大公約数を算出する
-func gcd(a, b int64) int64 {
-	if b == 0 {
-		return a
-	}
-	return gcd(b, a%b)
-}
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	scanner.Scan()
+	l, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+	scanner.Scan()
+	r, _ := strconv.ParseInt(scanner.Text(), 10, 64)
 
-// 絶対値を算出する
-func abs(a int64) int64 {
-	if a >= 0 {
-		return a
-	}
-	return -a
-}
-
-// スライスの要素を入れ替える
-func swap(array []int64, i, j int) {
-	array[i], array[j] = array[j], array[i]
-}
-
-// 順列を生成する
-func generate(n int, array []int64, result *[][]int64) {
-	if n == 1 {
-		tmp := make([]int64, len(array))
-		copy(tmp, array)
-		*result = append(*result, tmp)
-	} else {
-		for i := 0; i < n; i++ {
-			generate(n-1, array, result)
-			if n%2 == 1 {
-				swap(array, 0, n-1)
-			} else {
-				swap(array, i, n-1)
-			}
+	var ans int64 = 0
+	for i := int64(len(strconv.FormatInt(l, 10))); i <= int64(len(strconv.FormatInt(r, 10))); i++ {
+		low := max(l, int64(math.Pow10(int(i-1))))
+		high := min(r, int64(math.Pow10(int(i)))-1)
+		if low <= high {
+			ans += ((sum(high) - sum(low-1) + mod) % mod) * i % mod
+			ans %= mod
 		}
 	}
+
+	fmt.Println(ans)
 }
 
 func max(a, b int64) int64 {
@@ -68,27 +48,4 @@ func min(a, b int64) int64 {
 		return a
 	}
 	return b
-}
-
-// 総和を算出する
-func sum(a int64) int64 {
-	return (a % mod) * ((a + 1) % mod) / 2 % mod
-}
-
-const mod = 1000000007
-
-func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Split(bufio.ScanWords)
-	scanner.Scan()
-	l, _ := strconv.ParseInt(scanner.Text(), 10, 64)
-	scanner.Scan()
-	r, _ := strconv.ParseInt(scanner.Text(), 10, 64)
-
-	var ans int64 = 0
-	for i := int64(len(strconv.FormatInt(l, 10))); i <= int64(len(strconv.FormatInt(r, 10))); i++ {
-		ans += ((sum(min(r, int64(math.Pow10(int(i)))-1)) - sum(max(l-1, int64(math.Pow10(int(i-1)))-1))) % mod) * i % mod
-	}
-
-	fmt.Println(ans)
 }
