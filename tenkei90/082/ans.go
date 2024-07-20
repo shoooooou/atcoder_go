4d1efa3df31c45956b2c5c12df0142eb7dcb46d0
s.nakamura <shotenba@gmail.com>
2024-07-20 13:30:15 +0900

    典型問題082を途中コミット

--- tenkei90/082/ans.go
+++ tenkei90/082/ans.go
@@ -3,13 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 )
 
 // スライスの合計値を算出する
-func sumSlice(numbers []int) int {
-	sum := 0
+func sumSlice(numbers []int64) int64 {
+	var sum int64 = 0
 	for _, n := range numbers {
 		sum += n
 	}
@@ -17,7 +18,7 @@ func sumSlice(numbers []int) int {
 }
 
 // 最大公約数を算出する
-func gcd(a, b int) int {
+func gcd(a, b int64) int64 {
 	if b == 0 {
 		return a
 	}
@@ -25,7 +26,7 @@ func gcd(a, b int) int {
 }
 
 // 絶対値を算出する
-func abs(a int) int {
+func abs(a int64) int64 {
 	if a >= 0 {
 		return a
 	}
@@ -33,14 +34,14 @@ func abs(a int) int {
 }
 
 // スライスの要素を入れ替える
-func swap(array []int, i, j int) {
+func swap(array []int64, i, j int) {
 	array[i], array[j] = array[j], array[i]
 }
 
 // 順列を生成する
-func generate(n int, array []int, result *[][]int) {
+func generate(n int, array []int64, result *[][]int64) {
 	if n == 1 {
-		tmp := make([]int, len(array))
+		tmp := make([]int64, len(array))
 		copy(tmp, array)
 		*result = append(*result, tmp)
 	} else {
@@ -55,18 +56,38 @@ func generate(n int, array []int, result *[][]int) {
 	}
 }
 
+func max(a, b int64) int64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// 総和を算出する
+func sum(a int64) int64 {
+	return (a % mod) * ((a + 1) % mod) / 2 % mod
+}
+
+const mod = 1000000007
+
 func main() {
-	const mod = 1000000007
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(bufio.ScanWords)
 	scanner.Scan()
-	l, _ := strconv.Atoi(scanner.Text())
+	l, _ := strconv.ParseInt(scanner.Text(), 10, 64)
 	scanner.Scan()
-	r, _ := strconv.Atoi(scanner.Text())
+	r, _ := strconv.ParseInt(scanner.Text(), 10, 64)
 
-	ans := 0
-	for i := l; i <= r; i++ {
-		ans = (ans + (i%mod)*len(strconv.Itoa(i))) % mod
+	var ans int64 = 0
+	for i := int64(len(strconv.FormatInt(l, 10))); i <= int64(len(strconv.FormatInt(r, 10))); i++ {
+		ans += ((sum(min(r, int64(math.Pow10(int(i)))-1)) - sum(max(l-1, int64(math.Pow10(int(i-1)))-1))) % mod) * i % mod
 	}
 
 	fmt.Println(ans)
